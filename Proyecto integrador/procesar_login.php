e24b000d49055c11b0c7c7eaf45d28a2da427fc8
CitlaliHP <122800835+CitlaliHP@users.noreply.github.com>
2023-08-07 19:15:43 -0600

    Update procesar_login.php

--- Proyecto integrador/procesar_login.php
+++ Proyecto integrador/procesar_login.php
@@ -1,38 +1,58 @@
 <?php
+
+session_unset();
+session_destroy();
+
+session_start();
+
 // Parametros de conexión a la base de datos
 $servername = "127.0.0.1";
 $username = "root";
 $password = "12345";
 $dbname = "cafeteria";
 
-// Conexión a la base de datos
-$miConexion = new mysqli($servername, $username, $password, $dbname);
+try {
+    // Conexión a la base de datos
+    $miConexion = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
-if ($miConexion->connect_error) {
-    die("Error de conexión: " . $miConexion->connect_error);
+    // Verificar la conexión
+    if ($miConexion->connect_error) {
+        die("Error de conexión: " . $miConexion->connect_error);
+    }
+
+    // Obtener los valores de matrícula y contraseña del formulario
+    $matriculaForm = $_POST['matricula'];
+    $contraseñaForm = $_POST['contraseña'];
+
+    $miConsulta = "SELECT * FROM registros WHERE matricula='$matriculaForm'";
+
+    // Ejecutar la consulta
+    $result = $miConexion->query($miConsulta);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row['contraseña'];
+
+        // Verificar si la contraseña ingresada coincide con la contraseña almacenada (usando password_verify)
+        if (password_verify($contraseñaForm, $hashedPassword)) {
+            // Inicio de sesión exitoso
+            echo "Inicio de sesión exitoso";
+            $_SESSION["matricula"] = $matriculaForm;
+            // Redirigir a otra página después del inicio de sesión exitoso
+            header("Location: productos.php");
+            exit;
+        } else {
+            // Contraseña incorrecta
+            echo "Error al iniciar sesión: contraseña incorrecta";
+        }
+    } else {
+        // Matrícula incorrecta
+        echo "Error al iniciar sesión: matrícula no encontrada";
+    }
+
+    // Cerrar la conexión a la base de datos
+    $miConexion->close();
+} catch (Exception $e) {
+    echo 'Caught exception: ', $e->getMessage(), "\n";
 }
-
-// Obtener los valores de matrícula y contraseña del formulario
-$matriculaForm = $_POST['matricula'];
-$contraseñaForm = $_POST['contraseña'];
-
-$miConsulta = "SELECT * FROM registros WHERE matricula='$matriculaForm' AND contraseña='$contraseñaForm'";
-
-// Valida si la variable $result tiene un valor
-$result = $miConexion->query($miConsulta);
-
-if ($result->num_rows > 0) {
-    // Si hay un resultado, significa que la matrícula y la contraseña son correctas
-    echo "Inicio de sesión exitoso";
-    // Redirigir a otra página después del inicio de sesión exitoso
-    header("Location: productos.php");
-    exit;
-} else {
-    // Si no hay resultados, significa que la matrícula o la contraseña son incorrectas
-    echo "Error al iniciar sesión: matrícula o contraseña incorrectas";
-}
-
-// Cerrar la conexión a la base de datos
-$miConexion->close();
 ?>
